82e575610800ca9f01053df9eea0df56775f96cc
AlexIvan-AG <wolfodonnell1934@gmail.com>
2023-09-22 00:30:51 -0500

    inicio

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<?php require 'controlador.php';?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/hash.css">
+    <title>Hash</title>
+</head>
+<body>
+
+    <form id="segundo" action="" method="POST" autocomplete="off" >
+        <h1>Registro de Usuario</h1>
+
+        <label for="lab2">Usuario</label>
+        <input id="inp2" type="text" name="usuario_usu" id="">
+
+        <label id="l2" for="">Password</label>
+        <input id="i2" type="password" name="password_usu" id="">
+        
+        <button id="b2" type="submit" name="registro">Registro</button>
+    </form>
+
+</body>
+</html>
